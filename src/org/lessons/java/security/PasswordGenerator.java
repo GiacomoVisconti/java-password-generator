1a07e135106d6680a8883ad3b305756a59adbff3
GiacomoVisconti <giacomovisconti1999@gmail.com>
2025-10-01 09:48:41 +0200

    corrections

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -14,47 +14,36 @@ public class PasswordGenerator {
         // Esempio:
         // Ho un utente che si chiama Pinco Pallo, nato il 12/05/1994, il cui colore preferito è il magenta. La sua password sarà Pinco-Pallo-magenta-2011
 
+        Scanner input = new Scanner(System.in);
 
         //Input Nome
         System.out.println("Inserisci il tuo nome:");
-
-        Scanner inputNome = new Scanner(System.in);
-
-        String nome = inputNome.nextLine();
+        String nome = input.nextLine();
 
 
         //Input cognome    
         System.out.println("Inserisci il tuo cognome:");
-
-        Scanner inputCognome = new Scanner(System.in);
-
-        String cognome = inputCognome.nextLine();
+        String cognome = input.nextLine();
         
 
 
         //Input Colore preferito
         System.out.println("Qual è il tuo colore preferito?");
-
-        Scanner inputColore = new Scanner(System.in);
-
-        String colore = inputColore.nextLine();
+        String colore = input.nextLine();
 
 
         //Input Data di nascita
         System.out.println("Inserisci il tuo giorno di Nascita:");
-        Scanner dayInput = new Scanner(System.in);
-        int day = dayInput.nextInt();
+        int day = input.nextInt();
         String d = String.format("%02d", day );
 
 
          System.out.println("Inserisci il tuo mese di Nascita:");
-        Scanner monthInput = new Scanner(System.in);
-        int month = monthInput.nextInt();
+        int month = input.nextInt();
         String m = String.format("%02d", month );
 
         System.out.println("Inserisci il tuo anno di Nascita con questo formato 'yyyy': ");
-        Scanner yearInput = new Scanner(System.in);
-        int year = yearInput.nextInt();
+        int year = input.nextInt();
         
 
         String data = d + "/" + m + "/" + year;
